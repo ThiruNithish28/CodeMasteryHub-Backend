9197252726872d1890d336d081761699af1e4523
aswinsrini <aswinsrinivasan2004@gmail.com>
2024-11-20 15:17:12 +0530

    initial commit

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ app.get("/hello", (req, res) => {
   res.send("backend is working fine");
 });
 
+app.post("/login", (req, res) => {
+  //login functionality
+});
+app.post("/signup", (req, res) => {
+  //signup functionality
+});
 app.listen(3000, () => {
   console.log("connected to the port 3000....");
 });
